6f160485bcd114aee974f032bfdd3b1ecb3d43be
Noe Trevino <noetrevino.music@gmail.com>
2024-07-25 01:19:22 -0500

    change database structure
    - no more enum
    - i got this off youtube

---
+++ backend/src/main/java/backend/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import backend.models.Role;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
