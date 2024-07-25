84d1c939f2ad0405e8c87798025ade5d7e40195c
Noe Trevino <noetrevino.music@gmail.com>
2024-07-25 12:39:37 -0500

    configure authentication

--- backend/src/main/java/backend/security/SecurityConfig.java
+++ backend/src/main/java/backend/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf().disable().cors().and()
                 .exceptionHandling().authenticationEntryPoint(authEntryPoint).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
